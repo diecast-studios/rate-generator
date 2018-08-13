ca6b3f662586fd937ab0ad313cd55ddd1d1cb01c
Diecast Studios <41753963+diecast-studios@users.noreply.github.com>
2018-08-13 14:54:08 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -29,14 +29,16 @@ var hoursWorked = 0;
 var hourlyRate = 0;
 
 $("#business-overhead-text").html("<b>Expenses: </b> $" + costSum);
-
+$('#exampleModalCenter').hide();
 
 // Click event to add group members to firebase 
 $("#calculate-rate").on("click", function (event) {
     event.preventDefault();
 
     if ($("#health-insurance").val() === "" || $("#auto-insurance").val() === "" || $("#software").val() === "" || $("#etc").val() === "" || $("#business-rent").val() === "" || $("#business-insurance").val() === "" || $("#business-etc").val() === "" || $("#rent").val() === "" || $("#utilities").val() === "" || $("#employment-rate").val() === "" || $("#tax-rate").val() === "" || $("#processing-rate").val() === "" || $("#hours-worked").val() === "") {
-        alert("Please don't forget to input a value for all inputs. If you have no cost, input 0.");
+        //$('#myInput').trigger('focus');
+        $('#\\#myModal').modal('show');
+
     } else {
 
     // First Section
@@ -45,10 +47,9 @@ $("#calculate-rate").on("click", function (event) {
     software = parseInt(document.getElementById("software").value);
     etc = parseInt(document.getElementById("etc").value);
 
-    console.log(healthInsurance + autoInsurance + software + etc);
     costSum = healthInsurance + autoInsurance + software + etc;
 
-    $("#business-overhead-text").html("<b>Expenses: </b> $" + costSum);
+    $("#business-overhead-text").html("<b>Expenses: </b> $" + costSum.toFixed(2));
 
     // Second Section
     businessRent = parseInt(document.getElementById("business-rent").value);
@@ -56,7 +57,7 @@ $("#calculate-rate").on("click", function (event) {
     businessEtc = parseInt(document.getElementById("business-etc").value);
 
     overhead = businessRent + businessInsurance + businessEtc;
-    $("#second-overhead").html("<b>Expenses: </b> $" + overhead);
+    $("#second-overhead").html("<b>Expenses: </b> $" + overhead.toFixed(2));
 
     // Third Section
     rent = parseInt(document.getElementById("rent").value);
@@ -66,7 +67,7 @@ $("#calculate-rate").on("click", function (event) {
     var costAll = costSum + overhead + inPocket;
     pocketOverhead;
     $("#in-pocket").html("<b>In Pocket: </b> $" + inPocket);
-    $("#pocket-overhead").html("<b>In Pocket + Overhead: </b> $" + costAll);
+    $("#pocket-overhead").html("<b>In Pocket + Overhead: </b> $" + costAll.toFixed(2));
 
     // Fourth Section
     employmentRate = parseInt(document.getElementById("employment-rate").value);
@@ -74,14 +75,12 @@ $("#calculate-rate").on("click", function (event) {
     processingRate = parseInt(document.getElementById("processing-rate").value);
 
     var incRequired = costAll / (employmentRate / 100) / (1 - (taxRate / 100)) /(1 - (processingRate / 100));
-    console.log(incRequired);
-    $("#inc-required").html("<b>Income Required: </b> $" + incRequired);
+    $("#inc-required").html("<b>Income Required: </b> $" + incRequired.toFixed(2));
 
     // Final Spot
     hoursWorked = parseInt(document.getElementById("hours-worked").value);
     hourlyRate = incRequired / hoursWorked;
-    console.log(hourlyRate);
-    $("#hourly-rate").html("<b>Hourly Rate: </b> $" + hourlyRate);
+    $("#hourly-rate").html("<b>Hourly Rate: </b> $" + hourlyRate.toFixed(2));
     
 
 }
